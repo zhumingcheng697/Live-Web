6b77f69a3109346099a4e8a35887393ad2d97d26
McCoy Zhu <mz1945@nyu.edu>
2022-04-02 02:00:54 -0400

    ADD: `eighty-six` controls

--- outline-chat/86.js
+++ outline-chat/86.js
@@ -1,9 +1,28 @@
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
+  const tools = document.getElementById("tools");
+  const canvasArea = document.getElementById("canvas-area");
+  const inputs = tools.getElementsByTagName("input");
+  const spans = tools.getElementsByTagName("span");
+
+  let selected = null;
+
   function checkToolsHeight() {
     document.documentElement.style.setProperty(
       "--tools-height",
-      document.getElementById("tools").clientHeight + "px"
+      tools.clientHeight + "px"
     );
+  }
+
+  function hideTools() {
+    selected = null;
+    document.body.classList.add("no-canvas-selected");
+    for (let input of inputs) {
+      input.disabled = true;
+    }
+
+    for (let span of spans) {
+      span.classList.add("disabled");
+    }
   }
 
   window.addEventListener("resize", () => {
@@ -12,29 +31,86 @@ window.addEventListener("load", () => {
 
   checkToolsHeight();
 
-  const canvasArea = document.getElementById("canvas-area");
+  document.body.addEventListener("click", (e) => {
+    if (e.target === document.body || e.target.id === "main-area") {
+      hideTools();
+    }
+  });
 
-  for (let image of document.getElementsByTagName("img")) {
-    const canvas = document.createElement("canvas");
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    canvas.className = "eighty-six";
+  tools.addEventListener("click", (e) => {
+    if (e.target === tools) {
+      hideTools();
+    }
+  });
 
-    const threshold = 12;
-    const margin = 8;
+  window.addEventListener("load", () => {
+    for (let image of document.getElementsByTagName("img")) {
+      const canvas = document.createElement("canvas");
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
+      canvas.className = "eighty-six";
 
-    const context = canvas.getContext("2d");
+      let threshold = 12;
+      let margin = 8;
 
-    canvasArea.append(canvas);
+      const context = canvas.getContext("2d");
+      canvasArea.append(canvas);
 
-    context.drawImage(image, 0, 0);
+      function draw() {
+        context.drawImage(image, 0, 0);
 
-    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        const imageData = context.getImageData(
+          0,
+          0,
+          canvas.width,
+          canvas.height
+        );
 
-    context.putImageData(
-      detectEdge(threshold, margin, imageData, context, 1),
-      0,
-      0
-    );
-  }
+        context.putImageData(
+          detectEdge(threshold, margin, imageData, context, 1),
+          0,
+          0
+        );
+      }
+
+      draw();
+
+      function showTools() {
+        selected = image.src;
+        document.body.classList.remove("no-canvas-selected");
+        for (let input of inputs) {
+          input.disabled = false;
+        }
+
+        inputs[0].value = threshold;
+        inputs[1].value = margin;
+
+        for (let span of spans) {
+          span.classList.remove("disabled");
+        }
+      }
+
+      inputs[0].addEventListener("input", () => {
+        if (selected === image.src) {
+          threshold = +inputs[0].value;
+          draw();
+        }
+      });
+
+      inputs[1].addEventListener("input", () => {
+        if (selected === image.src) {
+          margin = +inputs[1].value;
+          draw();
+        }
+      });
+
+      canvas.addEventListener("click", () => {
+        if (selected === image.src) {
+          hideTools();
+        } else {
+          showTools();
+        }
+      });
+    }
+  });
 });
