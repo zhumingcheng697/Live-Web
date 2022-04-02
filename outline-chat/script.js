a4c8880ec8561cff5658769e5503dd388646ba23
McCoy Zhu <mz1945@nyu.edu>
2022-04-02 00:02:42 -0400

    UPDATE: using `DOMContentLoaded` instead of `load`

--- outline-chat/script.js
+++ outline-chat/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
   const videoArea = document.getElementById("video-area");
   const videosDiv = document.getElementById("videos");
 
